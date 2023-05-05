8513626f54e07e6092bdef371235a77eff265915
George <gwwebster23@gmail.com>
2023-05-05 18:03:19 -0500

    React controlled components lab

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+
+  const [newItemField, setNewItemField] = useState("");
+  const [newItemCategory, setNewItemCategory] = useState("Produce");
+
+  function handleNewItem(e) {
+    e.preventDefault()
+    const newItem = {
+      id: uuid(),
+      name: newItemField,
+      category: newItemCategory,
+    }
+    onItemFormSubmit(newItem)
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleNewItem}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={newItemField} onChange={(e) => setNewItemField(e.target.value)} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={newItemCategory} onChange={(e) => setNewItemCategory(e.target.value)} >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
@@ -24,3 +38,8 @@ function ItemForm(props) {
 }
 
 export default ItemForm;
+
+// create state variables for both category dropdown and text input in this component (CHECK)
+// make callback fn in app component, send down to this one (CHECK)
+// on submit, make new item obj in this component, send up to app component (CHECK)
+// then, update stateful items array of objs in App with the new item obj (CHECK)
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,33 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, itemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  const itemsToDisplay = items
+  .filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
+  })
+  .filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm 
+        onItemFormSubmit={itemFormSubmit} 
+      />
+      <Filter 
+        search={search} 
+        onSearchChange={setSearch}
+        selectedCategory={selectedCategory} 
+        onCategoryChange={handleCategoryChange} 
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
